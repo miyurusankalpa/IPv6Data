07fd9fba0f570dfb40ce705065f6dfbae2f50308
miyuru <miyuru@live.com>
2019-10-04 15:32:36 +0000

    new steam data

--- steam/script.sh
+++ steam/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 codes="maa1 lhr1 ams1 sgp1 sto1 sto2 fra1 par1 iad1 hkg1 lax1 mad1 ord1 lax1 okc1 atl1 sea1 vie1 tyo1 syd1 gru1"
-eval "(echo "Code,IPv4,IPv6") >> output.csv"
+eval "(echo "Domain,IPv4,IPv6") >> output.csv"
 
 for code in $codes; do
 		e=0
---
+++ steam/script2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+eval "(echo "Domain,IPv4,IPv6") >> output2.csv"
+
+for n in {0..3999} ; do
+				domain="valve$n.steamcontent.com"
+				echo "$domain"
+				
+				C=$(dig +short $domain -t A @2001:4860:4860::8888)
+
+				OUT4="dig +short $domain -t A @2606:4700:4700::1111"
+				OUT6="dig +short $domain -t AAAA @2606:4700:4700::1001"
+				
+				if [ -z $C ]
+					then
+						continue
+				fi
+				
+				eval "(echo $domain; echo ","; $OUT4; echo ","; $OUT6;) | tr '\n' ' ' >> output2.csv"
+				eval "(echo "") >> output2.csv"
+					sleep .2
+done
