e9e4b45de1a97053a14439fd1f885af1fcbd7d4f
Miyuru Sankalpa <msankalpa@gmail.com>
2020-01-12 11:14:35 +0530

    steam jan 2020 data
    renamed scipt file name, output copied so the dif can be viewed

--- steam/script2.sh
+++ steam/script2.sh
@@ -6,7 +6,8 @@ for n in {0..3999} ; do
 				domain="valve$n.steamcontent.com"
 				echo "$domain"
 				
-				C=$(dig +short $domain -t A @2001:4860:4860::8888)
+				#C=$(dig +short $domain -t A @2001:4860:4860::8888)
+				C=$(dig +short $domain -t A @2402:4000::6)
 
 				OUT4="dig +short $domain -t A @2606:4700:4700::1111"
 				OUT6="dig +short $domain -t AAAA @2606:4700:4700::1001"
